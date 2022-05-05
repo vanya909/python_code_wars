8299ae43ce3043b7c6a6ad7f11a6e258e6954b2c
vanya909 <vanya909x75@gmail.com>
2022-05-05 13:36:50 +0700

    Update palindrome string kata

--- fourth_kyu/One Line Task - Palindrome String/palindrome_string.py
+++ fourth_kyu/One Line Task - Palindrome String/palindrome_string.py
@@ -1,6 +1,8 @@
+# These don't work
 #def palindrome(n, c): return (c*n)[:int(n/2+n%2)]+(c*n)[int(n/2)-1::-1]
-def palindrome(n,c):return c+c[0]*(n-len(c)*2)+c[::-1][n%2:]
+#def palindrome(n,c):return c+c[0]*(n-len(c)*2)+c[::-1][n%2:]
+
+# Working version
 palindrome=lambda n,c:c+c[-1]*(n-len(c)*2+1)+c[-2::-1]
 #-----------------------------------------------------|
-print(palindrome(3, 'ab'))
-print(palindrome(20, "abcd"))
+# 55 characters
